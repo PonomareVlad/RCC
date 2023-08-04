a1525440a55b8a1bef3303e171a3e464372b0000
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-08-05 02:20:58 +0500

    Enhance page layout and usability for mobile browsers
    This commit introduces better support for mobile and touch devices. A theme color and icon were added, and viewport settings improved to enhance the mobile browsing experience. Support for iOS standalone mode was also added. User Select is disabled to prevent intrusive behaviors for touch events, and the logo has undergone a scaling transformation for better presentation on small screens. Clickable elements have been updated with a pointer cursor to represent interactiveness.

--- public/page.mjs
+++ public/page.mjs
@@ -7,8 +7,10 @@ export function* template({title, body}) {
     <head>
         <meta charset="UTF-8">
         <title>${title}</title>
+        <meta name="theme-color" content="#070707">
         <meta content="ie=edge" http-equiv="X-UA-Compatible">
-        <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        <meta content="yes" name="apple-mobile-web-app-capable">
+        <meta content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
               name="viewport">
         <script type="importmap">${JSON.stringify(importmap, null, 2)}</script>
         <script async crossorigin="anonymous"
@@ -17,6 +19,8 @@ export function* template({title, body}) {
         <script crossorigin="anonymous" referrerpolicy="no-referrer"
                 integrity="sha512-j7t6vlvFm1ANrX6dGt+NFtZqOjWeeNlPmAWCkf8mf1n7CMjZVubwUBEvNiVT0RE7voXE7ojiQBNWsAreLg00CA=="
                 src="https://cdnjs.cloudflare.com/ajax/libs/logtail-browser/0.3.0/dist/umd/logtail.js"></script>
+        <link rel="icon" href="/images/logo.svg" sizes="any" type="image/svg+xml">
+        <link rel="mask-icon" href="/images/logo.svg" color="#070707">
         <link href="/fonts/stylesheet.css" rel="stylesheet">
         <link href="/styles/reset.css" rel="stylesheet">
         <link href="/styles/vars.css" rel="stylesheet">
