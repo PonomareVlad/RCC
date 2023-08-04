bf46a99cf6f2c52af19a7c5ff7f7e84e4d50f14e
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-08-04 22:37:01 +0500

    Add Logtail for error logging
    This commit adds the Logtail client-side JavaScript library to the webpage. This library makes it possible to have better error logging. A new script tag, pointing to the Logtail library, has been added in page.mjs. In client.mjs, new event listeners have been added to handle error events and communicate with the Logtail service when necessary. These changes will allow us to collect detailed error information and improve application reliability.

--- public/client.mjs
+++ public/client.mjs
@@ -1,4 +1,15 @@
 import "@lit-labs/ssr-client/lit-element-hydrate-support.js";
 import {App} from "./app.mjs";
 
+if (Logtail in window) {
+    const logtail = new Logtail("1qvKAa4X3qErJjuwfv6ji2jk");
+    window.addEventListener("pagehide", logtail.flush.bind(logtail));
+    window.addEventListener("error",
+        ({type, message, source, lineno, colno, error} = {}) => {
+            void logtail.error(message, {type, message, source, lineno, colno, error});
+            return false;
+        }
+    );
+}
+
 App.define();
