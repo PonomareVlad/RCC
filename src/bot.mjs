01a4599045bd3212d3b5aba9af43fa6bd7abc4fa
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-08-15 21:33:34 +0500

    Add stage selection and user actions to bot
    This commit enhances the bot's functionalities including a stage selection mechanism and commands to delete existing votes and users. On message reception, the bot now checks if the input matches an existing stage. If so, it activates that stage and sends a confirming reply. If the special commands 'Сброс этапов,' 'Удалить голоса,' or 'Удалить юзеров' are received, the stages are reset, votes are deleted, or users are deleted, respectively. Keyboard and database modules have been added to support these new features.

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,4 +1,6 @@
-import {Bot} from "grammy";
+import {ObjectId} from "bson";
+import {Bot, Keyboard} from "grammy";
+import {accounts, stages, votes} from "./db.mjs";
 
 export const {
 
@@ -13,5 +15,64 @@ export const {
 // Default grammY bot instance
 export const bot = new Bot(token);
 
-// Sample handler for a simple echo bot
-bot.on("message:text", ctx => ctx.reply(ctx.msg.text));
+bot.on("message:text", async (ctx, next) => {
+    await ctx.replyWithChatAction("typing");
+    const stages = await getStages();
+    if (ctx.msg.text in stages) {
+        const stage = stages[ctx.msg.text];
+        await selectStage(stage);
+        const reply_markup = await getKeyboard();
+        return ctx.reply(`Активирован этап ${ctx.msg.text}`, {reply_markup});
+    } else switch (ctx.msg.text) {
+        case "Сброс этапов": {
+            await selectStage();
+            const reply_markup = await getKeyboard();
+            return ctx.reply(`Этапы сброшены`, {reply_markup});
+        }
+        case "Удалить голоса": {
+            await votes.deleteMany({});
+            const reply_markup = await getKeyboard();
+            return ctx.reply(`Все голоса удалены`, {reply_markup});
+        }
+        case "Удалить юзеров": {
+            await accounts.deleteMany({});
+            const reply_markup = await getKeyboard();
+            return ctx.reply(`Все юзеры удалены`, {reply_markup});
+        }
+    }
+    return next();
+});
+
+bot.on("message:text", async ctx => ctx.reply("Выберите необходимое действие в меню", {
+    reply_markup: await getKeyboard()
+}));
+
+async function getStages() {
+    const stagesData = await stages.find().toArray();
+    const getButton = ({active, rounds = []}) => [
+        (active ? "✅" : ""),
+        rounds.at(0),
+        (rounds.length > 1 ? `(+${rounds.length - 1})` : "")
+    ].filter(Boolean).join(" ");
+    return Object.fromEntries(stagesData.map(stage => [getButton(stage), stage]))
+}
+
+async function selectStage({_id: id} = {}) {
+    const activate = {$set: {active: true}};
+    const deactivate = {$set: {active: false}};
+    if (!id) return stages.updateMany({}, deactivate).then(console.debug);
+    const _id = new ObjectId(id);
+    await stages.updateOne({_id}, activate).then(console.debug);
+    await stages.updateMany({_id: {$not: {$in: [_id]}}}, deactivate).then(console.debug);
+}
+
+async function getKeyboard() {
+    const stages = await getStages();
+    const buttons = [
+        ...Object.keys(stages),
+        "Сброс этапов",
+        "⚠️ Удалить голоса",
+        "⚠️ Удалить юзеров"
+    ];
+    return Keyboard.from([buttons]).resized().persistent().toFlowed(2);
+}
