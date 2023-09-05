f1a5ff6fb608995571b854f9c390ff579de8d3f4
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-09-06 04:42:41 +0500

    fix

--- public/livereload.mjs
+++ public/livereload.mjs
@@ -6,6 +6,8 @@ if (
 async function init(
     url = `http://${location.hostname}:8080/livereload`
 ) {
+    const response = await fetch(url);
+    if (!response.ok) return;
     const source = new EventSource(url);
     const reload = () => location.reload();
     source.onerror = () => (source.onopen = reload);
